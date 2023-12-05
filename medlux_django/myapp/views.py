7b09a70eb672e3374a59a36d9c3e81da1b661d8e
Sc00ra <scoorabartek@gmail.com>
2023-12-05 15:46:49 +0100

    od nowa

--- medlux_django/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def strona1(request):
-    return render(request, 'strona1.html')
-
-def strona2(request):
-    return render(request, 'strona2.html')
-
