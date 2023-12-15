28622e982cdd837f11d58814aaace3b829a481e6
Sc00ra <scoorabartek@gmail.com>
2023-12-15 20:27:23 +0100

    Login page
    dodanie folderu templates gdzie dodawac bedziemy strony w html i folder static gdzie beda dodawane css dla danego html

--- medlux_projekt/medlux_projekt/urls.py
+++ medlux_projekt/medlux_projekt/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("pracownicy/", include("pracownicy.urls")),
+    path("pracownicy/", include("pracownicy.urls")), # url do bazy pracownikow
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls"))  #url do logowania
 ]
